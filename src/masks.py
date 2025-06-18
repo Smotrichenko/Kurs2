65ff05becf34b38a757876cceae7800560bb47f2
Max_Smotr <smotrichenko@yandex.ru>
2025-06-18 20:43:26 +0300

    init

--- src/masks.py
+++ src/masks.py
@@ -7,14 +7,6 @@ def get_mask_card_number(number_card: str) -> str:
     return f"{part_1} {part_2}{mask_card} {part_3}"
 
 
-number_card = input("Введите номер карты: ")
-print(get_mask_card_number(number_card))
-
-
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
     return f"**{account_number[-4:]}"
-
-
-account_number = input("Введите номер счета: ")
-print(get_mask_account(account_number))
