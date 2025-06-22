c1ed007051a7045b4241a64d35a5948abdf25742
Max_Smotr <smotrichenko@yandex.ru>
2025-06-22 23:25:34 +0300

    Добавил функцию sort_by_date в модуле processing

--- src/processing.py
+++ src/processing.py
@@ -3,7 +3,8 @@ from typing import Any, Dict, List
 
 def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """Функция принимает список словарей и возвращает новый список словарей,содержащий только те словари,
-    у которых ключ state соответствует указанному значению - 'EXECUTED'"""
+    у которых ключ state соответствует указанному значению - 'EXECUTED'
+    """
 
     filtered_list_of_dict = []
     for key in data:
@@ -14,11 +15,9 @@ def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List
     return filtered_list_of_dict
 
 
-data = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-result = filter_by_state(data)
-print(result)
+def sort_by_date(data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """Функция принимает список словарей и возвращает новый список словарей с сортировкой по убыванию
+    по ключу 'data'
+    """
+    sort_list_of_dict = sorted(data, key=lambda x: x["date"], reverse=True)
+    return sort_list_of_dict
