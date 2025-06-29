56912ced5c29ee5bfaf962312f6fab6841c3c853
Max_Smotr <smotrichenko@yandex.ru>
2025-06-29 20:46:29 +0300

    Добавил тесты и успешно протестировал модуль masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,14 @@
 def get_mask_card_number(number_card: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
+    if not isinstance(number_card, str) or len(number_card) == 0:
+        return "Введите номер карты"
+    if not number_card.isdigit():
+        return "Номер счета должен содержать только цифры"
+    if len(number_card) < 16:
+        return "Недостаточная длина номера карты"
+    if len(number_card) > 16:
+        return "Слишком длинный номер карты"
+
     part_1 = number_card[:4]
     part_2 = number_card[4:6]
     mask_card = "** ****"
@@ -9,4 +18,8 @@ def get_mask_card_number(number_card: str) -> str:
 
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
+    if len(account_number) != 20:
+        return "Введите 20-значный номер счета"
+    if not account_number.isdigit():
+        return "Номер счета должен содержать только цифры"
     return f"**{account_number[-4:]}"
