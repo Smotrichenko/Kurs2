95ee4b6f2a8ca8ded2f696850c266e6e3a90aa95
Max_Smotr <smotrichenko@yandex.ru>
2025-07-14 23:29:10 +0300

    Написал декоратор @log

---
+++ src/decorators.py
@@ -0,0 +1,41 @@
+import datetime
+from functools import wraps
+
+
+def log(filename=None):
+    """
+    Декоратор, который будет автоматически логировать начало и конец выполнения функции,
+    а также ее результаты или возникшие ошибки.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            message_start = f"Начало выполнения функции {func.__name__}:{start_time}\n"
+            print(message_start)
+            if filename:
+                with open(filename, "a", encoding="utf-8") as f:
+                    f.write(message_start)
+            try:
+                result = func(*args, **kwargs)
+                end_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                message_end = f"Окончание выполнения функции {func.__name__}:{end_time}\n" f"Результат: {result}\n"
+                print(message_end)
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as f:
+                        f.write(message_end)
+                        f.write("-" * 52 + "\n")
+                return result
+            except Exception as e:
+                with open(filename, "a", encoding="utf-8") as f:
+                    f.write(
+                        f"Ошибка в функции {func.__name__} при вызове с аргументами: {args}, {kwargs}.\n"
+                        f"Ошибка {e}\n"
+                    )
+                    f.write("-" * 52 + "\n")
+                raise
+
+        return wrapper
+
+    return decorator
