4a36df6688833eb828123aaba24b15a5f2eb89bc
Max_Smotr <smotrichenko@yandex.ru>
2025-07-15 19:22:57 +0300

    Исправил замечания наставника

--- src/decorators.py
+++ src/decorators.py
@@ -13,27 +13,39 @@ def log(filename=None):
         def wrapper(*args, **kwargs):
             start_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             message_start = f"Начало выполнения функции {func.__name__}:{start_time}\n"
-            print(message_start)
+
             if filename:
                 with open(filename, "a", encoding="utf-8") as f:
                     f.write(message_start)
+            else:
+                print(message_start)
+
             try:
                 result = func(*args, **kwargs)
                 end_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 message_end = f"Окончание выполнения функции {func.__name__}:{end_time}\n" f"Результат: {result}\n"
-                print(message_end)
+
                 if filename:
                     with open(filename, "a", encoding="utf-8") as f:
                         f.write(message_end)
                         f.write("-" * 52 + "\n")
+                else:
+                    print(message_end)
+
                 return result
+
             except Exception as e:
-                with open(filename, "a", encoding="utf-8") as f:
-                    f.write(
-                        f"Ошибка в функции {func.__name__} при вызове с аргументами: {args}, {kwargs}.\n"
-                        f"Ошибка {e}\n"
-                    )
-                    f.write("-" * 52 + "\n")
+                error_message = (
+                    f"Ошибка в функции {func.__name__} при вызове с аргументами: {args}, {kwargs}.\n" f"Ошибка {e}\n"
+                )
+
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as f:
+                        f.write(error_message)
+                        f.write("-" * 52 + "\n")
+                else:
+                    print(error_message)
+
                 raise
 
         return wrapper
