45e29ec7091c975cdf002c36057661896c680a04
Max_Smotr <smotrichenko@yandex.ru>
2025-07-23 23:39:32 +0300

    Добавил логгирование к модулям masks, external_api, utils

--- src/external_api.py
+++ src/external_api.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from typing import Dict
 
@@ -7,6 +8,19 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://api.apilayer.com/exchangerates_data/convert"
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+log_file_path = os.path.join(LOG_DIR, "utils.log")
+logger = logging.getLogger("API")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(log_file_path, mode='w', encoding="utf=8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def convert_to_rub(operation_amount: Dict) -> float:
@@ -29,7 +43,9 @@ def convert_to_rub(operation_amount: Dict) -> float:
             raise ValueError(f"Ошибка при запуске курса валют: {response.text}")
 
         data = response.json()
+        logger.debug("Конвертация прошла успешно")
         return float(data.get("result", 0.0))
-    except (requests.RequestException, Exception) as e:
-        print(f"Ошибка при конвертации валюты: {e}")
+    except (requests.RequestException, Exception) as ex:
+        logger.error(f"Произошла ошибка: {ex}")
+        print(f"Ошибка при конвертации валюты: {ex}")
         return 0.0
